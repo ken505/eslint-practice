8529fe0a68ea71d2da01f14d0fb3f4eb3fcc4c0e
ken505 <68226398+ken505@users.noreply.github.com>
2021-07-07 17:10:17 +0900

    ✨ Fix a eslitrc error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,20 +8,24 @@ module.exports = {
     ecmaFeatures: {
       // jsx: true,
     },
-    // ecmaVersion: 2021,
+    // ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  // plugins: ["react"],
   rules: {
-    // suppress errors for missing 'import React' in files
-    // ファイルに「importReact」がない場合のエラーを無視
     "react/react-in-jsx-scope": "off",
-
-    // allow jsx syntax in js files ( for next.js project )
-    // js ファイルで jsx 構文を許可する（ next.js プロジェクトの場合）
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    //should add ".ts" if typescript project
-    // typescript プロジェクトの場合は「.ts」を追加する必要があります
     "react/prop-types": "off",
   },
 };
+
+// suppress errors for missing 'import React' in files
+// ファイルに「importReact」がない場合のエラーを無視
+// "react/react-in-jsx-scope": "off",
+
+// allow jsx syntax in js files ( for next.js project )
+// js ファイルで jsx 構文を許可する（ next.js プロジェクトの場合）
+// "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+//should add ".ts" if typescript project
+// typescript プロジェクトの場合は「.ts」を追加する必要があります
+
+// "react/prop-types": "off",
